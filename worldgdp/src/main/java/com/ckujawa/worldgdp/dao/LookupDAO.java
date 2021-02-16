84b210f465e62d95141c4a46879a3e099f3c4d8f
Chris Kujawa <ckujawa@chriskujawadev.com>
2021-02-16 17:12:56 -0500

    work on filtering county grid display

--- worldgdp/src/main/java/com/ckujawa/worldgdp/dao/LookupDAO.java
+++ worldgdp/src/main/java/com/ckujawa/worldgdp/dao/LookupDAO.java
@@ -1,11 +1,13 @@
 package com.ckujawa.worldgdp.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 @Service
 public class LookupDAO {
@@ -13,17 +15,26 @@ public class LookupDAO {
 	@Autowired 
 	NamedParameterJdbcTemplate namedParamJdbcTemplate;
 	
-	public List<String> getContinents(){
-		MapSqlParameterSource params = new MapSqlParameterSource();
+	public List<String> getContinents(Map<String, Object> params){
 		return namedParamJdbcTemplate.queryForList("SELECT DISTINCT continent FROM country "
-				+ " ORDER BY continent", 
+				+ (StringUtils.hasText((String)params.get("region")) 
+						? "where region = :region" 
+						: " ORDER BY continent"), 
 				params, String.class);
 	}
 	
-	public List<String> getRegions(){
+	public List<String> getContinentForRegion(String region){
 		MapSqlParameterSource params = new MapSqlParameterSource();
+		params.addValue("region", region);
+		return namedParamJdbcTemplate.queryForList("SELECT DISTINCT continent FROM country " +
+				"WHERE region = :region", params, String.class);
+	}
+	
+	public List<String> getRegions(Map<String, Object> params){
 		return namedParamJdbcTemplate.queryForList("SELECT DISTINCT region FROM country "
-				+ " ORDER BY region", 
+				+ (StringUtils.hasText((String)params.get("continent")) ? 
+					" where continent = :continent" : 
+					" ORDER BY region"), 
 				params, String.class);
 	}
 	
